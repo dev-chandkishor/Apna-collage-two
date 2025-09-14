48cbc7c3038a6ff9cd24246072a7112ea55f730a
Chandkishor <hannarybrus1234@gmail.com>
2025-09-14 12:30:44 +0530

    mereging sentece

--- app.js
+++ app.js
@@ -2,3 +2,4 @@
 //add new feature -from
 //agar hamse kisi bhi new branch ki upstream branch banane ko bole to uska comman 
 //git push --set-upstream origin "branch name"
+//git merge featrure to ham ese yaha se bhi kar sakte hai
